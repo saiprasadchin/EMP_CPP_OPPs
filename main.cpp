88c045c9bfc68a406631d1747f591f55d79d0d90
“saiprasadchin” <saiprasadchindam@gmail.com>
2020-08-26 06:48:30 -0700

    Added : Code for employee attendence

--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
 #include<iostream>
+#include <ctime>
 
 using namespace std;
 
+void checkAttendance()
+{
+    srand(time(0));
+    int attendence = (rand() % 2);
+    if(attendence == 1) {
+        cout << "\nEmployee is Present\n" << endl;
+    }
+    else {
+        cout << "\nEmployee is Absent\n" << endl;
+    }  
+
+}
 int main()
 {
     cout <<"\nWelcome to Employee Wage Problem\n";
+    checkAttendance();
     return 0;
 }
