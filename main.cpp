556e263208134da4c000c922a65ef15e19d667b5
“saiprasadchin” <saiprasadchindam@gmail.com>
2020-08-26 08:41:46 -0700

    Added : Code for calculate monthly wage

--- main.cpp
+++ main.cpp
@@ -5,31 +5,42 @@ using namespace std;
 const int WAGE_PER_HOUR = 20;
 const int FULL_DAY_HOUR = 8;
 const int PART_TIME_HOUR = 4;
+const int WORK_DAYS_IN_MONTH = 20;
 
 int checkAttendance()
 {
-    srand(time(0));
     int attendence = ( rand() % 3 );
     return attendence;
 }
-void calculateDailyWage()
+int calculateDailyWage()
 {
+    int wage = 0;
     switch( checkAttendance() ) {
         case 1:
-            cout << "Employee is Full Time\n";
-            cout << "Wage = " << WAGE_PER_HOUR * FULL_DAY_HOUR;
+            wage = WAGE_PER_HOUR * FULL_DAY_HOUR;
             break;
         case 2:
-            cout << "Employee is Part Time\n";
-            cout << "Wage = " << WAGE_PER_HOUR * PART_TIME_HOUR;
+            wage = WAGE_PER_HOUR * PART_TIME_HOUR;
             break;
         default:
-            cout << "Employee is Absent\n";
+            wage = 0;
     }
+    return wage;
+}
+void calculateMonthlyWages()
+{
+    srand(time(0));
+    int monthlyWage = 0;
+    for( int day = 1; day <= WORK_DAYS_IN_MONTH; day++ ) {
+        int dailyWage = calculateDailyWage(); 
+        cout << "Day"<< day <<"    wages : "<< dailyWage << endl;
+        monthlyWage += dailyWage;
+    }
+    cout << "\n\nMonthly Wage = " << monthlyWage << endl;
 }
 int main()
 {
     cout <<"\nWelcome to Employee Wage Problem\n";
-    calculateDailyWage();
+    calculateMonthlyWages();
     return 0;
 }
