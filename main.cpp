59ff47618bc4eca4fedd0e252698a366a5394bb6
“saiprasadchin” <saiprasadchindam@gmail.com>
2020-08-26 07:26:53 -0700

    Added : Code for calculate daily wage

--- main.cpp
+++ main.cpp
@@ -2,22 +2,33 @@
 #include <ctime>
 
 using namespace std;
+const int WAGE_PER_HOUR = 20;
+const int FULL_DAY_HOUR = 8;
+const int EMPLOYEE_PRESENT = 1;
 
-void checkAttendance()
+int checkAttendance()
 {
     srand(time(0));
     int attendence = (rand() % 2);
-    if(attendence == 1) {
-        cout << "\nEmployee is Present\n" << endl;
+    if( attendence == EMPLOYEE_PRESENT ) {
+        cout << "\nEmployee is Present";
+        return attendence;
     }
-    else {
-        cout << "\nEmployee is Absent\n" << endl;
-    }  
-
+    cout << "\nEmployee is Absent";
+    return attendence;
+     
+}
+void calculateDailyWage()
+{
+    if ( checkAttendance() == EMPLOYEE_PRESENT ) {
+        cout << "\nEmployee Wage = "<< WAGE_PER_HOUR * FULL_DAY_HOUR << endl;
+    } else {
+        cout << "\nEmployee Wage = "<< 0 << endl;
+    }
 }
 int main()
 {
     cout <<"\nWelcome to Employee Wage Problem\n";
-    checkAttendance();
+    calculateDailyWage();
     return 0;
 }
